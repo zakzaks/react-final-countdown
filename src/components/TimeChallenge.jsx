88117a71d356d91b4da9fb538ad24a830517042e
zakzaks <ariefn82@gmail.com>
2026-03-19 08:26:35 +0700

    Add ResultModel component and integrate it into TimeChallenge for displaying results

--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import ResultModel from "./ResultModel";
 
 export default function TimeChallenge({ title, targetTime }) {
 	const [timerStarted, setTimerStarted] = useState(false);
@@ -17,20 +18,23 @@ export default function TimeChallenge({ title, targetTime }) {
 	}
 
 	return (
-		<section className="challenge">
-			<h2>{title}</h2>
-			{timerExpired && <p>You Lost!</p>}
-			<p className="challenge-time">
-				{targetTime} second{targetTime > 1 ? "s" : ""}
-			</p>
-			<p>
-				<button onClick={timerStarted ? handleStop : handleStart}>
-					{timerStarted ? "Stop" : "Start"} Challenge
-				</button>
-			</p>
-			<p className={timerStarted ? "active" : undefined}>
-				{timerStarted ? "Time is running ...." : "Timer inactive"}
-			</p>
-		</section>
+		<>
+			{timerExpired && <ResultModel targetTime={targetTime} result="lost" />}
+			<section className="challenge">
+				<h2>{title}</h2>
+				{timerExpired && <p>You Lost!</p>}
+				<p className="challenge-time">
+					{targetTime} second{targetTime > 1 ? "s" : ""}
+				</p>
+				<p>
+					<button onClick={timerStarted ? handleStop : handleStart}>
+						{timerStarted ? "Stop" : "Start"} Challenge
+					</button>
+				</p>
+				<p className={timerStarted ? "active" : undefined}>
+					{timerStarted ? "Time is running ...." : "Timer inactive"}
+				</p>
+			</section>
+		</>
 	);
 }
